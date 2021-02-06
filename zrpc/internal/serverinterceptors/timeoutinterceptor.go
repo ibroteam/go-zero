e08fc22711f397bc981d501518f6fe90fa9ff875
jasyaf <jasyaf@dingtalk.com>
2021-02-06 19:44:43 +0800

    RPC报错堆栈信息提取

--- zrpc/internal/serverinterceptors/timeoutinterceptor.go
+++ zrpc/internal/serverinterceptors/timeoutinterceptor.go
@@ -2,6 +2,9 @@ package serverinterceptors
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"reflect"
 	"time"
 
 	"github.com/tal-tech/go-zero/core/contextx"
@@ -13,6 +16,20 @@ func UnaryTimeoutInterceptor(timeout time.Duration) grpc.UnaryServerInterceptor 
 		handler grpc.UnaryHandler) (resp interface{}, err error) {
 		ctx, cancel := contextx.ShrinkDeadline(ctx, timeout)
 		defer cancel()
-		return handler(ctx, req)
+		// @CRACK begin
+		// 2021-02-06 by hujiachao
+		// 当RPC运行时遇到异常，将其报出来
+		result, err := handler(ctx, req)
+		if err != nil {
+			hostname, _ := os.Hostname()
+			title := "error@" + hostname
+			content := fmt.Sprintf("%+v\n\nREQ:\n%s\n%+v\n", err, reflect.TypeOf(req).String(), req)
+
+			fmt.Println(title)
+			fmt.Println(content)
+		}
+		return result, err
+		// return handler(ctx, req)
+		// @CRACK end
 	}
 }
