2cc63df72c13c6e1f1aafb7b91965d40c77c564b
yinhao <tfzxyinhao@gmail.com>
2021-04-01 11:46:13 +0800

    增加条件编译降低trace的侵入性

--- core/stores/sqlx/stmtctx_prod.go
+++ core/stores/sqlx/stmtctx_prod.go
@@ -1,4 +1,4 @@
-// +build !prod
+// +build prod
 
 package sqlx
 
@@ -6,15 +6,10 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
 )
 
 func execWithCtx(ctx context.Context, conn sessionConn, q string, args ...interface{}) (sql.Result, error) {
 	stmt := formatForPrint(q, args)
-	span, _ := opentracing.StartSpanFromContext(ctx, "db")
-	defer span.Finish()
-	ext.DBStatement.Set(span, stmt)
 
 	result, err := conn.Exec(q, args...)
 	if err != nil {
@@ -26,9 +21,6 @@ func execWithCtx(ctx context.Context, conn sessionConn, q string, args ...interf
 
 func execStmtWithCtx(ctx context.Context, conn stmtConn, args ...interface{}) (sql.Result, error) {
 	stmt := fmt.Sprint(args...)
-	span, _ := opentracing.StartSpanFromContext(ctx, "db")
-	defer span.Finish()
-	ext.DBStatement.Set(span, stmt)
 
 	result, err := conn.Exec(args...)
 	if err != nil {
@@ -39,9 +31,6 @@ func execStmtWithCtx(ctx context.Context, conn stmtConn, args ...interface{}) (s
 
 func queryWithCtx(ctx context.Context, conn sessionConn, scanner func(*sql.Rows) error, q string, args ...interface{}) error {
 	stmt := fmt.Sprint(args...)
-	span, _ := opentracing.StartSpanFromContext(ctx, "db")
-	defer span.Finish()
-	ext.DBStatement.Set(span, stmt)
 
 	rows, err := conn.Query(q, args...)
 	if err != nil {
@@ -54,9 +43,6 @@ func queryWithCtx(ctx context.Context, conn sessionConn, scanner func(*sql.Rows)
 
 func queryStmtWithCtx(ctx context.Context, conn stmtConn, scanner func(*sql.Rows) error, args ...interface{}) error {
 	stmt := fmt.Sprint(args...)
-	span, _ := opentracing.StartSpanFromContext(ctx, "db")
-	defer span.Finish()
-	ext.DBStatement.Set(span, stmt)
 
 	rows, err := conn.Query(args...)
 	if err != nil {
